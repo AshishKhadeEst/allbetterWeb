6625a4672d85ef85f36a0bf577ce49d8cd5dfc15
AshishKhadeEst <121441236+AshishKhadeEst@users.noreply.github.com>
2023-08-07 13:36:33 +0530

    removed tabs

--- page_objects/base.py
+++ page_objects/base.py
@@ -6,24 +6,17 @@ class Base:
         self.driver=driver
         self.EF=Elem_Func(self.driver)
 
-    Admin_tab=(By.XPATH,"//a[contains(@href,'viewAdminModule')]")
-    Pim_tab=(By.XPATH,By.XPATH,"//a[contains(@href,'viewPimModule']")
-    leave_tab=(By.XPATH,"//a[contains(@href,'viewLeaveModule']")
-    User_profile=(By.CLASS_NAME, 'oxd-userdropdown')
-    Log_out=(By.LINK_TEXT, "Logout")
 
-    def click_user_profile(self):
-        self.EF.find_element(self.User_profile).click()
+    # def click_user_profile(self):
 
-    def click_logout_link(self):
-        self.EF.find_element(self.Log_out).click()
+    # def click_logout_link(self):
 
-    def logout(self):
-        self.click_user_profile()
-        self.click_logout_link()
+    # def logout(self):
+    #     self.click_user_profile()
+    #     self.click_logout_link()
 
-    def navigate_to_tab(self,tab_link):
-        self.EF.find_element(tab_link).click()
+    # def navigate_to_tab(self,tab_link):
+
 
 
 
